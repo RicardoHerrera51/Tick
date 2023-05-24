f17850f9e3febbec9ae4683f384cf9598ae1d7ce
Ricardo Herrera <00143220@uca.edu.sv>
2023-05-24 02:05:17 -0600

    Prueba de gitignore

---
+++ Monotrigliceridos/src/main/java/com/rponce/Monotrigliceridos/MonotrigliceridosApplication.java
@@ -0,0 +1,13 @@
+package com.rponce.Monotrigliceridos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonotrigliceridosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonotrigliceridosApplication.class, args);
+	}
+
+}
---
+++ Monotrigliceridos/src/test/java/com/rponce/Monotrigliceridos/MonotrigliceridosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rponce.Monotrigliceridos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonotrigliceridosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
